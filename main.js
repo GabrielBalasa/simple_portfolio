8a01afe03932270cf49dd6ac0b657263577b51ff
Ionut Balasa <ionutgbalasa@gmail.com>
2020-04-27 18:04:09 +0100

    simple-portfolio

---
+++ main.js
@@ -0,0 +1,9 @@
+/* SCRIPTS.JS */
+
+function changePic() {
+  document.getElementById("profilePic").src = "profileChange.png";
+}
+
+function normalPic() {
+  document.getElementById("profilePic") .src = "profile.jpg";
+}
